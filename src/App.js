d172d65505d4a853b551eba631949d201e193a6e
Justinnn07 <justinv2006@gmail.com>
2021-10-20 21:59:56 +0500

    second commit :fire:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [totalData, setTotalData] = useState([]);
+
+  useEffect(() => {
+    const todos = localStorage.getItem("todo");
+    setTotalData(JSON.parse(todos));
+  }, []);
+
+  const addTodo = () => {
+    const todo = {
+      id: uuidv4(),
+      todo: input,
+    };
+    totalData.push(todo);
+    localStorage.setItem("todo", JSON.stringify(totalData));
+    setInput("");
+  };
+
+  const Delete = (id) => {
+    const filtered = totalData.filter((res) => res.id !== id);
+    localStorage.setItem("todo", JSON.stringify(filtered));
+    setTotalData(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ height: "60vh" }}>
+      <div style={{ display: "flex" }}>
+        <div className="input-group">
+          <span className="input-group-text">Enter Task</span>
+          <textarea
+            className="form-control"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
+        </div>
+        <div
+          class="btn-group"
+          role="group"
+          aria-label="Basic checkbox toggle button group"
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="checkbox"
+            class="btn-check"
+            id="btncheck1"
+            autocomplete="off"
+          />
+          <label
+            class="btn btn-outline-primary"
+            for="btncheck1"
+            onClick={addTodo}
+          >
+            Add Task
+          </label>
+        </div>
+      </div>
+      {totalData.map((res) => {
+        return (
+          <div class="card" style={{ width: "18rem" }}>
+            <div class="card-body">
+              <h5 class="card-title">{res.todo}</h5>
+              <a class="btn btn-primary" onClick={() => Delete(res.id)}>
+                Delete
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
